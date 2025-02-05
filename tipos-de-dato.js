15cab08695239d6490ffe0531a335a81aa718cd6
Selene Vargas <vargasselene99@gmail.com>
2025-02-05 15:51:41 -0600

    primeros cambios en mi archivo js para entregar mi primer proyecto

---
+++ tipos-de-dato.js
@@ -0,0 +1,28 @@
+// Asi se ponen los comentarios
+
+console.log("Hola mundo")
+
+// Declaracion de variables y asignaciòn de valores
+var nombre = "Ana"; // string
+var edad = 23; // Number
+var sueldo = 1234; // Number
+var casada = false; // Boolean
+var vacunas = true; // Boolean
+var hijos = null; // Null
+var datos; // undefined
+
+//typeof se utiliza para saber el tipo de dato de 
+console.log(typeof(nombre));
+console.log(typeof(edad));
+console.log(typeof(sueldo));
+console.log(typeof(casada));
+console.log(typeof(vacunas));
+console.log(typeof(hijos));
+console.log(typeof(datos));
+
+//La semantica es importante, significado de las palabras
+var nombre = "Francia";
+var pais = 20; 
+
+ //NoN (Not a number) Es un valor especial que identifica que una operación matematica no es un numero
+ console.log (10 * "ana")
