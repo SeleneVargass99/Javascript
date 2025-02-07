dcd55418c9469625c55d104a1e219436a4c40c64
Selene Vargas <vargasselene99@gmail.com>
2025-02-06 20:26:03 -0600

    commiting changes of class 2

---
+++ semaforo.js
@@ -0,0 +1,14 @@
+// Tipo de luz = verde, amarillo y rojo 
+// condicionales (Siga, detente, ALto )
+
+let tipoDeLuz = "amarilla";
+
+if (tipoDeLuz === "verde"){
+    console.log("si la luz es verde, siga adelante.")
+}else if (tipoDeLuz === "amarilla"){
+            console.log("si la luz es amarilla, detente.")
+}else if (tipoDeLuz === "roja"){
+    console.log("si la luz es roja, ALTO.")
+}else{
+    console.log("La luz no es correcta.")
+}
